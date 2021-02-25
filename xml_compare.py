83db7ee6a8249a7e69deccbb1e7704f6ff0e52ff
JohnnyBafak <JohnyBafak@users.noreply.github.com>
2021-02-25 20:16:11 +0100

    release implementation
    changed file structure to allow for publishing techtree

--- xml_compare.py
+++ xml_compare.py
@@ -15,6 +15,7 @@
 """
 __version__ = "0.2.1"
 NATIONS = [ "czech", "france", "germany", "china", "italy", "japan", "poland", "sweden", "uk", "usa", "ussr" ]
+GAME = {nation: {} for nation in NATIONS}
 
 from xml.etree import cElementTree as ET
 import sys, os, time
@@ -26,57 +27,56 @@ def getVersion(game):
         tree = ET.parse(path)
         p = tree.find("Paths")[0].text.split('mods/')[1]
         return p   
+        
+def readGame(WOT= "D:/World_of_Tanks_EU", IGR = True, bot=True, bob = True):
+    ver = getVersion(WOT)
+    fname = '{}/res_mods/{}_tankList.csv'.format( WOT, ver )
+    global GAME
+    if os.path.isfile( fname ):
+        with open(fname) as f:
+            delim = f.readline()[6]  
+            for line in f:
+                X = line.rstrip().split(delim)
+                nat = X[0]
+                name = X[1]
+                if IGR and "_IGR" in name: continue
+                if bot:
+                    if "_bot" in name: continue
+                    elif "_bootcamp" in name: continue
+                    elif "_training" in name: continue
+                if bob:
+                    if "_bob" in name: continue
+                    elif "_fallout" in name: continue
+                
+                val = { "lvl": X[5], "cls": X[6] , "gold": X[7] , "hid":X[9] }
+                GAME[nat][name] = val
+    else: raise IOError("[Errno 2] No such file or directory: '{}' Generate vehicle list using jb.getTank first".format(fname))
+    return ver
 
 class Compare():
-    
-    def __init__(self, name = "", folder = "D:/World_of_Tanks_EU", IGR = True, bot=True, bob = True):
+    def __init__(self, ver, name = "jbDefault"):
         self.LAYOUT = {}
-        self.GAME = {nation: {} for nation in NATIONS}
         for nation in NATIONS:
             self.LAYOUT[nation] = self.readLayout(nation, name)
         
-        ver = getVersion(folder)
-        self.readGame(folder, ver, IGR, bot, bob)
         self.checkDiff(name,ver)
         
         print "Done."
-
-    def readGame(self, WOT, ver, IGR, bot, bob):
-        fname = '{}/res_mods/{}_tankList.csv'.format( WOT, ver )
-        if os.path.isfile( fname ):
-            with open(fname) as f:
-                delim = f.readline()[6]  
-                for line in f:
-                    X = line.rstrip().split(delim)
-                    nat = X[0]
-                    name = X[1]
-                    if IGR and "_IGR" in name: continue
-                    if bot:
-                        if "_bot" in name: continue
-                        elif "_bootcamp" in name: continue
-                        elif "_training" in name: continue
-                    if bob:
-                        if "_bob" in name: continue
-                        elif "_fallout" in name: continue
-                    
-                    val = { "lvl": X[5], "cls": X[6] , "gold": X[7] , "hid":X[9] }
-                    self.GAME[nat][name] = val
-        else: raise IOError("[Errno 2] No such file or directory: '{}' Generate vehicle list using jb.getTank first".format(fname))
     
     def checkDiff(self, name, ver):
-        fname = './techtree/{}/compare.txt'.format(name)
+        fname = './xml/compare_{}.txt'.format(name)
         print "writting", fname
         with open (fname, "w") as f:
             f.write("xml_compare_missing for game version {}\n".format(ver))
             for nation in NATIONS:
-                inGame = list(self.GAME[nation].keys() )
+                inGame = list(GAME[nation].keys() )
                 XML = list(self.LAYOUT[nation])
                 newList = list(set(inGame) - set(XML))
                 oldList = list(set(XML) - set(inGame))
                 f.write("\n-T----class----gold--hidd-name------------- new {} [{}]\n".format(nation, len(newList) ) ) 
                 
                 for veh in newList:
-                    data = self.GAME[nation][veh]
+                    data = GAME[nation][veh]
                     f.write("{:>2} {:>10} {:>5} {:>5} {}\n".format(data['lvl'], data['cls'], data['gold'], data['hid'], veh ) )
                     #line_new = '{:>12}  {:>12}  {:>12}'.format(word[0], word[1], word[2])
                 f.write("xxxxxxxxxxxxxxxxxxxxxxxxxxxxx ------------- removed \n")
@@ -84,7 +84,7 @@ class Compare():
                     f.write(veh)
         
     def readLayout(self, nation, name):
-        xmls =  [ "./techtree/{}/{}-tree.xml", "./techtree/{}/{}-premium.xml" ]
+        xmls =  [ "./xml/{}/{}-tree.xml", "./xml/{}/{}-premium.xml" ]
         
         nationList = []        
         for path in xmls:
@@ -98,5 +98,13 @@ class Compare():
 
 
 if __name__ == "__main__":
-    Compare(*sys.argv[1:])
+    ver = readGame(*sys.argv[2:])
+    if len(sys.argv) > 1:
+        Compare(ver, sys.argv[1])
+    else: 
+        for name in os.listdir("./xml/"):
+            if os.path.isdir("./xml/"+name):
+                Compare(ver, name)
+            
+        
     time.sleep(1)
--- xml_export.py
+++ xml_export.py
@@ -1,118 +1,36 @@
 import tempfile, subprocess, os, shutil, zipfile, glob
 import xml.etree.ElementTree as ET
-import time
-
-# GLOBAL VARIABLES
-WOT = "D:/World_of_Tanks_EU"
-SOURCE = './techtree/'
-
+import time, sys
                    
-def getVersion():
+def getVersion(wot):
     """ Check paths.xml for current game version """
     p = 'fail'
-    path = WOT +'/paths.xml'
+    path = wot +'/paths.xml'
     tree = ET.parse(path)
     p = tree.find("Paths")[0].text.split('mods/')[1]
     return p   
     
 
-def main():
+def main(SOURCE = './xml/jbDefault', WOT = "D:/World_of_Tanks_EU"):
     # runtime
+    print "---    XML Export to game    ----"
+    print SOURCE, ">>", WOT
     cmd = raw_input("copy XML files with tree-shared.xml?")
 
     # Copy finish
-    destPath = WOT+'/res_mods/'+getVersion()+'/gui/flash/techtree/'
-    for file in  os.listdir("./techtree"):
+    destPath = WOT +'/res_mods/' + getVersion(WOT) + '/gui/flash/techtree/' 
+    print destPath
+    for file in  os.listdir(SOURCE):
         if file.endswith(".xml"):
-            shutil.copy2("techtree/"+file,destPath)
+            shutil.copy2(os.path.join(SOURCE,file),destPath)
     
     if len(cmd):
             shutil.copy2("tree-shared.xml",destPath)
 
     print "         Done, Tree-Shared-XML:", True if(len(cmd)) else False
-while True:
-    main()      
-            
-""" 
-
-        OLD 
-
-"""            
-            
-# AdvancedTechTree Layout Database Builder
-def build_LayData(arg):
-    global BUILD
-    BUILD = tempfile.mkdtemp()
-    DATAVER = 'databaseVersion.txt'
     
-    with open(DATAVER, 'r') as f:
-        number=int(f.read())
-    if len(arg) > 2:
-        if int(arg[2]) == 9:
-            # update version
-            number += 1
-    
-    with zipfile.ZipFile(BUILD+'/AdvancedTechtreeData.zip','w') as zf:
-        # write layouts
-        for f in glob.iglob('./Layouts/*/*.xml'):
-            zf.write(f, './LO/'+f.split('Layouts\\')[1])   
-            
-        # write nodesLibs
-        for f in glob.iglob('./NodeGraphic/*/*.swf'):
-            zf.write(f, './Nodes/'+f.split('NodeGraphic\\')[1])   
-            
-        # write data version
-        with open(BUILD + '/' + DATAVER, 'w') as f:
-            f.write('%d' % (number))
-        zf.write(BUILD + '/' + DATAVER, DATAVER)   
-    shutil.copy(BUILD+'/AdvancedTechtreeData.zip', './build/')
-    shutil.copy(BUILD + '/' + DATAVER, './')
-    print '[     ]   building techtree layout database.... (%s)' % number
-        
-    try:
-        type = int(arg[1])
-        if type == 1:
-            shutil.copy(BUILD + '/AdvancedTechtreeData.zip', './scripts/BACKUP/database/AdvancedTechtreeData_' + str(number) + '.zip')
-            print '[  B  ]   database backup created'
-        elif type > 1 and type < len(WOT)+1:
-            type -= 1
-            folder = WOT[type]+'/mods/resources/'
-            if not os.path.isdir(folder):
-                os.makedirs(folder)
-            shutil.copy(BUILD+'/AdvancedTechtreeData.zip',folder)
-            print '[     ]   transfered database in game %s' % WOT[type]
-        else: print '[     ]   no transfered database'
-    except Exception as E:
-        print "[  !  ]   transit error can't copy to %s (%s)" % (WOT[type], E)        
-        
-    build_finish(arg, number)
-    
-def build_finish(arg, version):
-    if os.path.isdir(BUILD):
-        shutil.rmtree(BUILD)
-    
-    if len(arg) > 2:
-        if int(arg[2]) == 9:
-            mod = int(arg[0]) - 1
-            if mod == 3:
-                name = './build/AdvancedTechtreeData.zip'
-                print '[ >>> ]   > releasing ATT Database %s' % version
-                shutil.copy('databaseVersion.txt', './# RELEASE/databaseVersion.txt')
-            else:
-                name =  './build/' + gINFO[mod]["id"] + '.wotmod'
-                print '[ >>> ]   > releasing', gINFO[mod]["id"], version
-            shutil.copy(name, './# RELEASE/')
-    
-            if mod == 0:
-                print '[ >>> ]   > building installer files...'
-                try:            
-                    #subprocess.check_call(['"D:\Program Files\32 Inno Setup 5/compil32.exe" /cc installScript_VerticalTechtree_quick.iss'])
-                    #subprocess.check_call(['"D:\Program Files\32 Inno Setup 5/compil32.exe" /cc installScript_VerticalTechtree.iss'])
-                    print '[  >  ]     > ATT.zip'
-                    install_zip()
-                    if INNO is not None:
-                        subprocess.check_call('installScript_run.bat "%s"' % INNO)
-                    else: 
-                        print '[  >  ]     ! no Inno-Setup Compiler'
-                except subprocess.CalledProcessError: print ' '
-         
+if __name__ == "__main__":
+    while True:
+        main(*sys.argv[1:])
+        time.sleep(1)     
+     
