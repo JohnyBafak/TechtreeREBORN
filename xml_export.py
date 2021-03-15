d683df105821583db53c4036f57e0d6cb4d276ea
JohnnyBafak <JohnyBafak@users.noreply.github.com>
2021-03-15 12:44:19 +0100

    Update xml_export.py

--- xml_export.py
+++ xml_export.py
@@ -1,41 +1,31 @@
-import tempfile, subprocess, os, shutil, zipfile, glob
+import os, shutil, time, sys
 import xml.etree.ElementTree as ET
-import time, sys
 
-def getVersion(wot):
-    """ Check paths.xml for current game version """
-    p = 'fail'
-    path = wot +'/paths.xml'
-    tree = ET.parse(path)
-    p = tree.find("Paths")[0].text.split('mods/')[1]
-    return p   
-    
 def copyXML(SOURCE, destPath):
     for file in  os.listdir(SOURCE):
         if file.endswith(".xml"):
             shutil.copy2(os.path.join(SOURCE,file),destPath)
     
-def main(SOURCE = './xml/jbDefault', WOT = "D:/World_of_Tanks_EU"):
-    # runtime
+def main(SOURCE = './xml/', WOT = "D:/World_of_Tanks_EU", name='_jbDefault'):
     print "---    XML Export to game    ----"
-    print SOURCE, ">>", WOT
-    cmd = raw_input("> copy XML files w/o tree-shared.xml?")
+    print SOURCE, ">>", WOT, "  -  ", name
 
-    # Copy finish
-    destPath = WOT +'/res_mods/' + getVersion(WOT) + '/gui/flash/techtree/' 
+    # Copy files in game
+    destPath = WOT +'/mods/configs/techtree/xml/dev/'
     print destPath
     if not os.path.exists(destPath):
         os.makedirs(destPath)
     copyXML(SOURCE,destPath)
-    if not len(cmd):
-        shutil.copy2("tree-shared.xml",destPath)
     
+    # release XML files 
     cmd = raw_input("> RELEASE XML files?")
     if len(cmd):
-        destPath = '../techtreeRelease/{}'.format(SOURCE[2:])
+        destPath = '../techtreeRelease/{}{}'.format(SOURCE[2:],name)
+        if not os.path.exists(destPath):
+            print destPath
+            os.makedirs(destPath)
         copyXML(SOURCE, destPath)
-        shutil.copy2("tree-shared.xml",destPath)
-    print "         Done, Tree-Shared-XML:", True if(len(cmd)) else False
+    print "         Done, release:", (True if(len(cmd)) else False) , "\n"
     
 if __name__ == "__main__":
     while True:
